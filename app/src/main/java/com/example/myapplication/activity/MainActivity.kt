a82335de876a9c61531cf3dd8130d1995bbd534a
lucalin-aos <lucalin19880129@gmail.com>
2026-05-10 22:24:06 +0800

    feat：建立activity package

--- app/src/main/java/com/example/myapplication/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/MainActivity.kt
@@ -1,8 +1,7 @@
-package com.example.myapplication
+package com.example.myapplication.activity
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -18,16 +17,15 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.R
 import com.example.myapplication.screen.CalendarScreen
 import com.example.myapplication.screen.HomeScreen
 import com.example.myapplication.screen.MoreScreen
--- app/src/main/java/com/example/myapplication/activity/SplashActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/SplashActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.myapplication.activity
 
 import android.annotation.SuppressLint
 import android.content.Intent
@@ -22,6 +22,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.myapplication.R
 import kotlinx.coroutines.delay
 
 @SuppressLint("CustomSplashScreen")
